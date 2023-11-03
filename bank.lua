03ccbe7d7964348a2a2d7a3e9002a6efe464ebde
Centralan <Jessegauthier070296@gmail.com>
2023-11-02 20:14:22 -0400

    bank.lua - value updates

--- bank.lua
+++ bank.lua
@@ -6,8 +6,8 @@ local bankGuardian = AI:new("VaultSec", "AI", "survival3");
 
 -- VAULT ENTRY 1
 
-local bank1_strikeLocation1 = Location:new("survival3", 19470, 61, -20756);
-local bank1_strikeLocation2 = Location:new("survival3", 19470, 61, -20754);
+local bank1_strikeLocation1 = Location:new("survival3", 19472, 61, -20755);
+local bank1_strikeLocation2 = Location:new("survival3", 19472, 61, -20755);
 function bank1_attempt(data)
         if bank1_checkCombo() then
                 bank1_lightButtons();
@@ -26,15 +26,15 @@ registerHook("INTERACT", "bank1_attempt", 143, "survival3", 19470, 63, -20759);
 local pressedButtons = {};
 
 local bank1_buttonSourceLocations = {
-        Location:new("survival3", 19470, 64, -20756);
-        Location:new("survival3", 19470, 64, -20757);
-        Location:new("survival3", 19470, 64, -20758);
-        Location:new("survival3", 19470, 63, -20756);
-        Location:new("survival3", 19470, 63, -20757);
-        Location:new("survival3", 19470, 63, -20758);
-        Location:new("survival3", 19470, 62, -20756);
-        Location:new("survival3", 19470, 62, -20757);
-        Location:new("survival3", 19470, 62, -20758);
+        Location:new("survival3", 19471, 64, -20759);
+        Location:new("survival3", 19472, 64, -20759);
+        Location:new("survival3", 19473, 64, -20759);
+        Location:new("survival3", 19471, 63, -20759);
+        Location:new("survival3", 19472, 63, -20759);
+        Location:new("survival3", 19473, 63, -20759);
+        Location:new("survival3", 19471, 62, -20759);
+        Location:new("survival3", 19472, 62, -20759);
+        Location:new("survival3", 19473, 62, -20759);
 };
 
 function bank1_shiftButtons(newDigit)
@@ -56,7 +56,7 @@ function bank1_lightButtons()
 
         for index, location in ipairs(bank1_buttonSourceLocations) do
                 if pButtons[index] == true then
-                        location:setBlock(152, 0);
+                        location:setBlock(101, 0);
                 else
                         location:setBlock(0, 0);
                 end
@@ -73,29 +73,32 @@ function bank1_checkCombo()
         return false;
 end
 
-local bank1_gateTriggerBlock = Location:new("survival3", 396, 67, -local bank1_gateBlocks = {};
-for x = 397, 401 do
-        for z = -898, -896 do
-                for y = 65, 67 do
-                        table.insert(bank1_gateBlocks, Location:new("survival3", x, y, z));
-                end
-        end
+local bank1_gateTriggerBlock = Location:new("survival3", 19470, 63, -20579);
+bank1_gateTriggerBlock:setBlock(0, 0); -- Reset incase we got stuck at some point.
+
+local bank1_gateBlocks = {};
+for x = 19469, 19469 do
+	for z = -20756, -20758 do
+		for y = 64, 62 do
+			table.insert(bank1_gateBlocks, Location:new("survival3", x, y, z));
+		end
+	end
 end
 
 function bank1_setGate(blockID)
-        for i = 1, #bank1_gateBlocks do
-                bank1_gateBlocks[i]:setBlock(blockID, 0);
-        end
+	for i = 1, #bank1_gateBlocks do
+		bank1_gateBlocks[i]:setBlock(blockID, 0);
+	end
 end
 
 function bank1_openGate()
-        bank1_gateTriggerBlock:setBlock(152, 0);
-        bank1_setGate(0);
+	bank1_gateTriggerBlock:setBlock(152, 0);
+	bank1_setGate(0);
 end
 
 function bank1_closeGate()
-        bank1_gateTriggerBlock:setBlock(0, 0);
-        bank1_setGate(101);
+	bank1_gateTriggerBlock:setBlock(0, 0);
+	bank1_setGate(101);
 end
 --registerHook("BLOCK_GAINS_CURRENT", "bank1_closeGate", "spawn", 402, 67, -908);
 
@@ -108,4 +111,3 @@ function bank1_button6() bank1_shiftButtons(6); end
 function bank1_button7() bank1_shiftButtons(7); end
 function bank1_button8() bank1_shiftButtons(8); end
 function bank1_button9() bank1_shiftButtons(9); end
-
