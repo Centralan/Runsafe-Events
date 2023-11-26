0a691d7b6f2293cd29843469ffb5a26d618d3488
Centralan <Jessegauthier070296@gmail.com>
2023-11-26 00:53:47 -0500

    holiday2023.lua

--- holiday2023.lua
+++ holiday2023.lua
@@ -14,22 +14,59 @@ end
 local sp = AI:new("SPENCER", "AI", "survival3");
 
 
----------------------------
-----------Player Processing--------
----------------------------
+-------------------
+--Intro Book ------
+-------------------
 
-function fireTick()
-	processPlayers({world:getPlayers()});
-end
 
 ---------------
 --Task 1 --
 ---------------
-function collect_snowballs(data)
-        local player = Player:new(data.player);
-                if player:hasItem("snowball", 64) then
-                sp:speak( player.name .. " has completed the [Classifed] objective.");
+--warp hunt1
+--18946,-21854
+
+function find_trees(data)
+          local player = Player:new(data.player);
+                 xmas:speak( player.name .. " has completed (classifed) objective.");
+                 player:addPermission("runsafe.event.trees");
   end
+
+registerHook("REGION_ENTER", "find_trees", "survival3-hunt_1");
+
+
+---------------
+--Task 2 --
+---------------
+--warp hunt2
+--18444,-17258
+
+local beanschest = Location:new(world, 18444.0, 62.0, -17258.0);
+local beanschestChestPlayers = {};
+local beanschestChestResetTimer = Timer:new("beans_reset_chest", 20 * 60 * 5);
+local beanschestChestResetTimerRunning = false;
+local beanschestChestOpen = Location:new(world, -64.0, 65.0, -515.0);
+
+function beans_snow(data)
+	local player = Player:new(data.player);
+	if not beanschestChestPlayers[player.name] then
+		beanschest:cloneChestToPlayer(player.name);
+		player:closeInventory();
+		xmas:speak( player.name .. " has completed (classifed) objective.");
+                player:addPermission("runsafe.event.beans");
+		beanschestChestPlayers[player.name] = true; 
+		
+		if not beanschestChestResetTimerRunning then
+			beanschestChestResetTimerRunning = true;
+			beanschestChestResetTimer:start();
+		end
+	end
 end
 
-registerHook("BLOCK_GAINS_CURRENT", "fireTick", "survival3", 19515.0, 64.0, -20801.0);
+function beanschest_reset_chest()
+	beanschestChestPlayers = {};
+	beanschestChestResetTimerRunning = false;
+end
+
+registerHook("INTERACT", "beans_snow", 61, "survival3", 18446, 66, -17257);
+
+registerHook("INTERACT", "beans_snow", 61, "survival3", 18446, 66, -17257);
