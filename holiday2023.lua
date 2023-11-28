d1df4b207dd9cb57b9f956f5c19116071735c5e1
Centralan <Jessegauthier070296@gmail.com>
2023-11-27 19:03:48 -0500

    holiday2023.lua - Rework task1 created task3

--- holiday2023.lua
+++ holiday2023.lua
@@ -11,13 +11,19 @@ function spawn_whisper(npc, msg, player)
 	p:sendMessage('&f&c' .. npc .. '&f' .. msg);
 end
 
-local xmas = AI:new("SPENCER", "ADMIN", "survival3");
+local xmas = AI:new("DOG", "AI", "survival3");
 
 
 -------------------
 --Intro Book ------
 -------------------
 
+-------------------
+--Event Perms ------
+-------------------
+--runsafe.event.trees
+--runsafe.event.emerald
+--runsafe.event.flower
 
 ---------------
 --Task 1 --
@@ -25,11 +31,30 @@ local xmas = AI:new("SPENCER", "ADMIN", "survival3");
 --warp hunt1
 --18946,-21854
 
+local framechest = Location:new(world, 18947.0, 112.0, -21580.0);
+local framechestChestPlayers = {};
+local framechestChestResetTimer = Timer:new("frame_reset_chest", 200 * 600 * 50);
+local framechestChestResetTimerRunning = false;
+local framechestChestOpen = Location:new(world, -64.0, 65.0, -515.0);
+
+
 function find_trees(data)
           local player = Player:new(data.player);
-                 xmas:speak( player.name .. " has completed (classifed) objective.");
+	    if not framechestChestPlayers[player.name] then
+		framechest:cloneChestToPlayer(player.name);
+		player:closeInventory();
+		xmas:speak( player.name .. " has completed the (classifed) objective.");
+	         player:sendMessage('&7Something seems weird about these trees, almost like the server made them.');
+	         player:sendMessage('&7Why would an Admin do this? Maybe I can find something here to build the portal.');
                  player:addPermission("runsafe.event.trees");
-  end
+		framechestChestPlayers[player.name] = true; 
+		
+		if not framechestChestResetTimerRunning then
+			framechestChestResetTimerRunning = true;
+			framechestChestResetTimer:start();
+		end
+	end
+end
 
 registerHook("REGION_ENTER", "find_trees", "survival3-hunt_1");
 
@@ -42,7 +67,7 @@ registerHook("REGION_ENTER", "find_trees", "survival3-hunt_1");
 
 local beanschest = Location:new(world, 18444.0, 62.0, -17258.0);
 local beanschestChestPlayers = {};
-local beanschestChestResetTimer = Timer:new("beans_reset_chest", 20 * 60 * 5);
+local beanschestChestResetTimer = Timer:new("beans_reset_chest", 200 * 600 * 50);
 local beanschestChestResetTimerRunning = false;
 local beanschestChestOpen = Location:new(world, -64.0, 65.0, -515.0);
 
@@ -51,8 +76,9 @@ function beans_snow(data)
 	if not beanschestChestPlayers[player.name] then
 		beanschest:cloneChestToPlayer(player.name);
 		player:closeInventory();
-		xmas:speak( player.name .. " has completed (classifed) objective.");
-                player:addPermission("runsafe.event.beans");
+		xmas:speak( player.name .. " has completed the (classifed) objective.");
+                player:sendMessage('&7Someone is into some weird stuff...');
+                player:addPermission("runsafe.event.emerald");
 		beanschestChestPlayers[player.name] = true; 
 		
 		if not beanschestChestResetTimerRunning then
@@ -69,4 +95,40 @@ end
 
 registerHook("INTERACT", "beans_snow", 61, "survival3", 18446, 66, -17257);
 
-registerHook("INTERACT", "beans_snow", 61, "survival3", 18446, 66, -17257);
+---------------
+--Task 3 --
+---------------
+--warp hunt3
+--37044,22486
+
+local flowerchest = Location:new(world, 37044.0, 34.0, -22486.0);
+local flowerchestChestPlayers = {};
+local flowerchestChestResetTimer = Timer:new("flower_reset_chest", 200 * 600 * 50);
+local flowerchestChestResetTimerRunning = false;
+local flowerchestChestOpen = Location:new(world, -64.0, 65.0, -515.0);
+
+function space_crash(data)
+	local player = Player:new(data.player);
+	if not flowerchestChestPlayers[player.name] then
+		flowerchest:cloneChestToPlayer(player.name);
+		player:closeInventory();
+		xmas:speak( player.name .. " has completed the (classifed) objective.");
+                player:sendMessage('&7Seems whatever caused this metor to crash left a ... flower?');
+		player:sendMessage('&7The flower has a strange glow to it. I wonder where it came from?');
+                player:addPermission("runsafe.event.flower");
+		flowerchestChestPlayers[player.name] = true; 
+		
+		if not flowerchestChestResetTimerRunning then
+			flowerchestChestResetTimerRunning = true;
+			flowerchestChestResetTimer:start();
+		end
+	end
+end
+
+function flowerchest_reset_chest()
+	flowerchestChestPlayers = {};
+	flowerchestChestResetTimerRunning = false;
+end
+
+registerHook("INTERACT", "space_crash", 77, "survival3", 37043, 33, -22481);
+
