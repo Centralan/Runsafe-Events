edb22575ae0fb3e747f5f9995d1c8ac57c18697c
Centralan <Jessegauthier070296@gmail.com>
2023-12-18 18:37:43 -0500

    Update Krimas.lua

--- Krimas.lua
+++ Krimas.lua
@@ -174,37 +174,30 @@ registerHook("REGION_ENTER", "end_event", "survival3-hunt_tp");
 --18946,-21854
 
 local framechest = Location:new(world, 18947.0, 112.0, -21580.0);
-local framechestChestPlayers = {};
-local framechestChestResetTimer = Timer:new("frame_reset_chest", 200 * 600 * 50);
-local framechestChestResetTimerRunning = false;
-local framechestChestOpen = Location:new(world, -64.0, 65.0, -515.0);
-
 
 function find_trees(data)
-          local player = Player:new(data.player);
-	    if not framechestChestPlayers[player.name] then
+local player = Player:new(data.player);
+        if not player:hasPermission("runsafe.event.1") then
 		framechest:cloneChestToPlayer(player.name);
 		player:closeInventory();
-		xmas:speak( player.name .. " has completed Trial 1.");
+		 xmas:speak( player.name .. " has completed Trial 1.");
 	         player:sendMessage('&7Something seems weird about these trees, almost like the server made them.');
 	         player:sendMessage('&7Why would an Admin do this? Maybe I can find something here to build the portal.');
-                 player:addPermission("runsafe.event.trees");
-		framechestChestPlayers[player.name] = true; 
-		
-		if not framechestChestResetTimerRunning then
-			framechestChestResetTimerRunning = true;
-			framechestChestResetTimer:start();
-		end
-	end
-end
+                 player:addPermission("runsafe.event.trees"); 
+                 player:addPermission("runsafe.event.1");
+				end
+			end
 
-function frame_reset_chest()
-	framechestChestPlayers = {};
-	framechestChestResetTimerRunning = false;
-end
-
-
+function find_trees2(data)
+local player = Player:new(data.player);
+         if player:hasPermission("runsafe.event.1") then
+	    player:sendMessage('&cYou have already completed this trial.');
+					end
+				end
 registerHook("REGION_ENTER", "find_trees", "survival3-hunt_1");
+registerHook("REGION_ENTER", "find_trees2", "survival3-hunt_1");
+
+
 
 
 ---------------
@@ -214,34 +207,26 @@ registerHook("REGION_ENTER", "find_trees", "survival3-hunt_1");
 --18444,-17258
 
 local beanschest = Location:new(world, 18444.0, 62.0, -17258.0);
-local beanschestChestPlayers = {};
-local beanschestChestResetTimer = Timer:new("beans_reset_chest", 200 * 600 * 50);
-local beanschestChestResetTimerRunning = false;
-local beanschestChestOpen = Location:new(world, -64.0, 65.0, -515.0);
-
+		
 function beans_snow(data)
 	local player = Player:new(data.player);
+        if not player:hasPermission("runsafe.event.2") then
 	if player:hasPermission("runsafe.event.igloo") then
-	if not beanschestChestPlayers[player.name] then
 		beanschest:cloneChestToPlayer(player.name);
 		player:closeInventory();
 		xmas:speak( player.name .. " has completed Trial 2.");
                 player:sendMessage('&7Someone is into some weird stuff...');
                 player:addPermission("runsafe.event.emerald");
-		beanschestChestPlayers[player.name] = true; 
-		
-		if not beanschestChestResetTimerRunning then
-			beanschestChestResetTimerRunning = true;
-			beanschestChestResetTimer:start();
-		end
-	end
-end
-end
-
-function beans_reset_chest()
-	beanschestChestPlayers = {};
-	beanschestChestResetTimerRunning = false;
-end
+                player:addPermission("runsafe.event.2");
+						end
+					end
+end		
+function beans_snow2(data)
+local player = Player:new(data.player);
+         if player:hasPermission("runsafe.event.2") then
+	    player:sendMessage('&cYou have already completed this trial.');
+					end
+				end
 
 function igloo_perm1(data)
 		local player = Player:new(data.player);
@@ -258,6 +243,8 @@ end
 registerHook("REGION_LEAVE", "igloo_perm2", "survival3-igloo");
 
 registerHook("PLAYER_ITEM_DROP", "beans_snow", "survival3", 264);
+registerHook("PLAYER_ITEM_DROP", "beans_snow2", "survival3", 264);
+
 
 ---------------
 --Task 3 --
@@ -266,14 +253,10 @@ registerHook("PLAYER_ITEM_DROP", "beans_snow", "survival3", 264);
 --37044,22486
 
 local flowerchest = Location:new(world, 37044.0, 34.0, -22486.0);
-local flowerchestChestPlayers = {};
-local flowerchestChestResetTimer = Timer:new("flower_reset_chest", 200 * 600 * 50);
-local flowerchestChestResetTimerRunning = false;
-local flowerchestChestOpen = Location:new(world, -64.0, 65.0, -515.0);
 
 function space_crash(data)
 	local player = Player:new(data.player);
-	if not flowerchestChestPlayers[player.name] then
+	        if not player:hasPermission("runsafe.event.3") then
 		flowerchest:cloneChestToPlayer(player.name);
 		player:closeInventory();
 		xmas:speak( player.name .. " has completed Trial 3.");
@@ -281,21 +264,19 @@ function space_crash(data)
 		player:sendMessage('&7The flower has a strange glow to it. I wonder where it came from?');
 		player:sendMessage('&9[?] &bSP&4E&bNCE&4R&f: &kbto wpt mt&f.');
                 player:addPermission("runsafe.event.flower");
-		flowerchestChestPlayers[player.name] = true; 
-		
-		if not flowerchestChestResetTimerRunning then
-			flowerchestChestResetTimerRunning = true;
-			flowerchestChestResetTimer:start();
-		end
-	end
-end
+		player:addPermission("runsafe.event.3");
+				end
+			end
 
-function flower_reset_chest()
-	flowerchestChestPlayers = {};
-	flowerchestChestResetTimerRunning = false;
-end
+function space_crash3(data)
+local player = Player:new(data.player);
+         if player:hasPermission("runsafe.event.3") then
+	    player:sendMessage('&cYou have already completed this trial.');
+					end
+				end
 
 registerHook("INTERACT", "space_crash", 77, "survival3", 37043, 33, -22481);
+registerHook("INTERACT", "space_crash3", 77, "survival3", 37043, 33, -22481);
 
 ---------------
 --Task 4 --
@@ -304,36 +285,29 @@ registerHook("INTERACT", "space_crash", 77, "survival3", 37043, 33, -22481);
 --20758,-21037
 
 local beaconchest = Location:new(world, 20746.0, 85.0, -21014.0);
-local beaconchestChestPlayers = {};
-local beaconchestChestResetTimer = Timer:new("beacon_reset_chest", 200 * 600 * 50);
-local beaconchestChestResetTimerRunning = false;
-local beaconchestChestOpen = Location:new(world, -64.0, 65.0, -515.0);
 
 function cpu_core(data)
 	local player = Player:new(data.player);
-	if not beaconchestChestPlayers[player.name] then
+	if not player:hasPermission("runsafe.event.4") then
 		beaconchest:cloneChestToPlayer(player.name);
 		player:closeInventory();
 		xmas:speak( player.name .. " has completed Trial 4.");
                 player:sendMessage('&7Who knew Runsafe ran on beacons.');
-		player:sendMessage('&9[?] &bS&4P&bENC&4ER&f: Hey &kthat &fbel&kon&fgs &kto m&fe.');
                 player:addPermission("runsafe.event.beacon");
-		beaconchestChestPlayers[player.name] = true; 
+		player:addPermission("runsafe.event.4"); 
+				end
+			end
 		
-		if not beaconchestChestResetTimerRunning then
-			beaconchestChestResetTimerRunning = true;
-			beaconchestChestResetTimer:start();
-		end
-	end
-end
-
-function beacon_reset_chest()
-	beaconchestChestPlayers = {};
-	beaconchestChestResetTimerRunning = false;
-end
+function cpu_core2(data)
+local player = Player:new(data.player);
+         if player:hasPermission("runsafe.event.4") then
+	    player:sendMessage('&cYou have already completed this trial.');
+					end
+				end
 
 registerHook("INTERACT", "cpu_core", 146, "survival3", 20737, 70, -21070);
-
+registerHook("INTERACT", "cpu_core2", 146, "survival3", 20737, 70, -21070);
+			
 ---------------
 --Task 5 --
 ---------------
@@ -341,34 +315,28 @@ registerHook("INTERACT", "cpu_core", 146, "survival3", 20737, 70, -21070);
 --19883, 22488
 
 local featherchest = Location:new(world, 19882.0, 62.0, -22489.0);
-local featherchestChestPlayers = {};
-local featherchestChestResetTimer = Timer:new("feather_reset_chest", 200 * 600 * 50);
-local featherchestChestResetTimerRunning = false;
-local featherchestChestOpen = Location:new(world, -64.0, 65.0, -515.0);
 
 function feather_reef(data)
 	local player = Player:new(data.player);
-	if not featherchestChestPlayers[player.name] then
+	if not player:hasPermission("runsafe.event.5") then
 		featherchest:cloneChestToPlayer(player.name);
 		player:closeInventory();
 		xmas:speak( player.name .. " has completed Trial 5.");
                 player:sendMessage('&7I wonder what this feather was used for...');
                 player:addPermission("runsafe.event.feather");
-		featherchestChestPlayers[player.name] = true; 
+		player:addPermission("runsafe.event.5");
+				end
+			end
 		
-		if not featherchestChestResetTimerRunning then
-			featherchestChestResetTimerRunning = true;
-			featherchestChestResetTimer:start();
-		end
-	end
-end
-
-function feather_reset_chest()
-	featherchestChestPlayers = {};
-	featherchestChestResetTimerRunning = false;
-end
+function feather_reef2(data)
+local player = Player:new(data.player);
+         if player:hasPermission("runsafe.event.5") then
+	    player:sendMessage('&cYou have already completed this trial.');
+					end
+				end
 
 registerHook("INTERACT", "feather_reef", 143, "survival3", 19881.0, 65.0, -22489);
+registerHook("INTERACT", "feather_reef2", 143, "survival3", 19881.0, 65.0, -22489);
 
 ---------------
 --Task 6 --
@@ -377,35 +345,28 @@ registerHook("INTERACT", "feather_reef", 143, "survival3", 19881.0, 65.0, -22489
 --spawn
 
 local dragonchest = Location:new(world, 19473.0, 70.0, -20817.0);
-local dragonchestChestPlayers = {};
-local dragonchestChestResetTimer = Timer:new("dragon_reset_chest", 200 * 600 * 50);
-local dragonchestChestResetTimerRunning = false;
-local dragonchestChestOpen = Location:new(world, -64.0, 65.0, -515.0);
 
 function dragon_spawn(data)
 	local player = Player:new(data.player);
-	if not dragonchestChestPlayers[player.name] then
+	if not player:hasPermission("runsafe.event.6") then
 		dragonchest:cloneChestToPlayer(player.name);
 		player:closeInventory();
 		xmas:speak( player.name .. " has completed Trial 6.");
                 player:sendMessage('&7Did DOG experience memory loss?');
                 player:addPermission("runsafe.event.dog");
-		dragonchestChestPlayers[player.name] = true; 
+		player:addPermission("runsafe.event.6"); 
+				end
+			end
 		
-		if not dragonchestChestResetTimerRunning then
-			dragonchestChestResetTimerRunning = true;
-			dragonchestChestResetTimer:start();
-		end
-	end
-end
-
-function dragon_reset_chest()
-	dragonchestChestPlayers = {};
-	dragonchestChestResetTimerRunning = false;
-end
+function dragon_spawn2(data)
+local player = Player:new(data.player);
+         if player:hasPermission("runsafe.event.6") then
+	    player:sendMessage('&cYou have already completed this trial.');
+					end
+				end
 
 registerHook("INTERACT", "dragon_spawn", 143, "survival3", 19473.0, 73.0, -20817.0);
-
+registerHook("INTERACT", "dragon_spawn2", 143, "survival3", 19473.0, 73.0, -20817.0);
 ---------------
 --Task 7 --
 ---------------
@@ -413,34 +374,28 @@ registerHook("INTERACT", "dragon_spawn", 143, "survival3", 19473.0, 73.0, -20817
 --pleco_city
 
 local plecochest = Location:new(world, 19478.0, 70.0, -20817.0);
-local plecochestChestPlayers = {};
-local plecochestChestResetTimer = Timer:new("pleco_reset_chest", 200 * 600 * 50);
-local plecochestChestResetTimerRunning = false;
-local plecochestChestOpen = Location:new(world, -64.0, 65.0, -515.0);
 
 function pleco_city(data)
 	local player = Player:new(data.player);
-	if not plecochestChestPlayers[player.name] then
+	if not player:hasPermission("runsafe.event.7") then
 		plecochest:cloneChestToPlayer(player.name);
 		player:closeInventory();
 		xmas:speak( player.name .. " has completed Trial 7.");
                 player:sendMessage('&7Man this city is almost majestic looking, it might even be the best.');
                 player:addPermission("runsafe.event.city");
-		plecochestChestPlayers[player.name] = true; 
+		player:addPermission("runsafe.event.7");
+				end
+			end
 		
-		if not plecochestChestResetTimerRunning then
-			plecochestChestResetTimerRunning = true;
-			plecochestChestResetTimer:start();
-		end
-	end
-end
-
-function pleco_reset_chest()
-	plecochestChestPlayers = {};
-	plecochestChestResetTimerRunning = false;
-end
+function pleco_city2(data)
+local player = Player:new(data.player);
+         if player:hasPermission("runsafe.event.7") then
+	    player:sendMessage('&cYou have already completed this trial.');
+					end
+				end
 
 registerHook("INTERACT", "pleco_city", 143, "creative", -1167.0, 249.0, -1406.0);
+registerHook("INTERACT", "pleco_city2", 143, "creative", -1167.0, 249.0, -1406.0);
 
 ---------------
 --Task 8 --
@@ -448,34 +403,27 @@ registerHook("INTERACT", "pleco_city", 143, "creative", -1167.0, 249.0, -1406.0)
 --skylimit
 
 local skychest = Location:new(world, 19534.0, 69.0, -20762.0);
-local skychestChestPlayers = {};
-local skychestChestResetTimer = Timer:new("sky_reset_chest", 200 * 600 * 50);
-local skychestChestResetTimerRunning = false;
-local skychestChestOpen = Location:new(world, -64.0, 65.0, -515.0);
-
+			
 function sky_limit(data)
 	local player = Player:new(data.player);
-	if not skychestChestPlayers[player.name] then
+	if not player:hasPermission("runsafe.event.8") then
 		skychest:cloneChestToPlayer(player.name);
 		player:closeInventory();
 		xmas:speak( player.name .. " has completed Trial 8.");
                 player:sendMessage('&7Thats a long way down.');
                 player:addPermission("runsafe.event.sky");
-		skychestChestPlayers[player.name] = true; 
-		
-		if not skychestChestResetTimerRunning then
-			skychestChestResetTimerRunning = true;
-			skychestChestResetTimer:start();
-		end
-	end
-end
-
-function sky_reset_chest()
-	skychestChestPlayers = {};
-	skychestChestResetTimerRunning = false;
-end
-
+		player:addPermission("runsafe.event.8");  
+				end
+			end
+			
+function sky_limit2(data)
+local player = Player:new(data.player);
+         if player:hasPermission("runsafe.event.8") then
+	    player:sendMessage('&cYou have already completed this trial.');
+					end
+				end
 registerHook("REGION_ENTER", "sky_limit", "survival3-hunt8");
+registerHook("REGION_ENTER", "sky_limit2", "survival3-hunt8");
 
 ---------------
 --Task 9 --
@@ -484,34 +432,27 @@ registerHook("REGION_ENTER", "sky_limit", "survival3-hunt8");
 --skally
 
 local skallychest = Location:new(world2, -21.0, 66.0, -209.0);
-local skallychestChestPlayers = {};
-local skallychestChestResetTimer = Timer:new("skally_reset_chest", 200 * 600 * 50);
-local skallychestChestResetTimerRunning = false;
-local skallychestChestOpen = Location:new(world2, -64.0, 65.0, -515.0);
 
 function skally_nether(data)
 	local player = Player:new(data.player);
-	if not skallychestChestPlayers[player.name] then
+	if not player:hasPermission("runsafe.event.9") then
 		skallychest:cloneChestToPlayer(player.name);
 		player:closeInventory();
 		xmas2:speak( player.name .. " has completed Trial 9.");
                 player:sendMessage('&7I wonder who Skalamander was?');
                 player:addPermission("runsafe.event.skally");
-		skallychestChestPlayers[player.name] = true; 
-		
-		if not skallyychestChestResetTimerRunning then
-			skallychestChestResetTimerRunning = true;
-			skallychestChestResetTimer:start();
-		end
-	end
-end
-
-function skally_reset_chest()
-	skallychestChestPlayers = {};
-	skallychestChestResetTimerRunning = false;
-end
+		player:addPermission("runsafe.event.9");  
+				end
+			end
+function skally_nether2(data)
+local player = Player:new(data.player);
+         if player:hasPermission("runsafe.event.9") then
+	    player:sendMessage('&cYou have already completed this trial.');
+					end
+				end
 
 registerHook("INTERACT", "skally_nether", 143, "survival3_nether", -21.0, 66.0, 211.0);
+registerHook("INTERACT", "skally_nether2", 143, "survival3_nether", -21.0, 66.0, 211.0);
 
 ---------------
 --Task 10 --
@@ -519,34 +460,28 @@ registerHook("INTERACT", "skally_nether", 143, "survival3_nether", -21.0, 66.0, 
 --warp hunt10(hunt10_1)
 
 local azchest = Location:new(world3, 10001.0, 63.0, 10003.0);
-local azchestChestPlayers = {};
-local azchestChestResetTimer = Timer:new("az_reset_chest", 200 * 600 * 50);
-local azchestChestResetTimerRunning = false;
-local azchestChestOpen = Location:new(world3, -64.0, 65.0, -515.0);
 
 function az_tower(data)
 	local player = Player:new(data.player);
-	if not azchestChestPlayers[player.name] then
+	if not player:hasPermission("runsafe.event.10") then
 		azchest:cloneChestToPlayer(player.name);
 		player:closeInventory();
 		xmas3:speak( player.name .. " has completed Trial 10.");
                 player:sendMessage('&7What even is the Azuren?');
                 player:addPermission("runsafe.event.az");
-		azchestChestPlayers[player.name] = true; 
+		player:addPermission("runsafe.event.10"); 
+				end
+			end
 		
-		if not azchestChestResetTimerRunning then
-			azchestChestResetTimerRunning = true;
-			azchestChestResetTimer:start();
-		end
-	end
-end
-
-function az_reset_chest()
-	azchestChestPlayers = {};
-	azchestChestResetTimerRunning = false;
-end
+function az_tower2(data)
+local player = Player:new(data.player);
+         if player:hasPermission("runsafe.event.10") then
+	    player:sendMessage('&cYou have already completed this trial.');
+					end
+				end
 
 registerHook("REGION_ENTER", "saz_tower", "azuren-hunt10_1");
+registerHook("REGION_ENTER", "saz_tower2", "azuren-hunt10_1");
 
 -------------------
 --Portal Check ------
@@ -1744,6 +1679,16 @@ function sm_end_r1()
 	   player:removePermission("runsafe.event.break");
 	   player:removePermission("runsafe.event.pray");
 	   player:removePermission("runsafe.warp.use.krimas");
+	   player:removePermission("runsafe.event.1");
+	   player:removePermission("runsafe.event.2");
+	   player:removePermission("runsafe.event.3");
+	   player:removePermission("runsafe.event.4");
+	   player:removePermission("runsafe.event.5");
+           player:removePermission("runsafe.event.6");
+	   player:removePermission("runsafe.event.7");
+	   player:removePermission("runsafe.event.8");
+	   player:removePermission("runsafe.event.9");
+	   player:removePermission("runsafe.event.10");
 end
 end
 end
