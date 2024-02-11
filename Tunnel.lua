6af646fd7a62ce1522a62b4bd797cba9ce2686df
Centralan <Jessegauthier070296@gmail.com>
2024-02-11 12:53:26 -0500

    Update Tunnel.lua

--- Tunnel.lua
+++ Tunnel.lua
@@ -1,27 +1,27 @@
 ------------------
---Locals--
+---- Tunnel 3 ----
 ------------------
 
 --World
-local world = World:new('tunnel');
+local world = World:new('tunnel2');
 local world2 = World:new('survival3');
 
 --tps
 local tunnelrespawn = Location:new(world, 1.0, 168.0, -22.0);
-tunnelrespawn:setYaw(-179.55);
-tunnelrespawn:setPitch(5.55);
+      tunnelrespawn:setYaw(-179.55);
+      tunnelrespawn:setPitch(5.55);
 
-local tunnelenter = Location:new(world, 1.0, 240.0, -22.0);
-tunnelenter:setYaw(-179.55);
-tunnelenter:setPitch(5.55);
+local tunnelenter = Location:new(world, 1.483, 240.0, -22.648);
+      tunnelenter:setYaw(-179.55);
+      tunnelenter:setPitch(5.55);
 
 local tunnelreset1 = Location:new(world, 1.407, 168.0, -289.634);
-tunnelreset1:setYaw(179.4);
-tunnelreset1:setPitch(3.8);
+      tunnelreset1:setYaw(179.4);
+      tunnelreset1:setPitch(3.8);
 
-local tunnelreset2 = Location:new(world, -06.99, 168.0, -303.667);
-tunne2reset1:setYaw(179.4);
-tunne2reset1:setPitch(9.2);
+local tunnelreset2 = Location:new(world, -0.580, 168.0, -290.634);
+      tunnelreset2:setYaw(179.4);
+      tunnelreset2:setPitch(3.8);
 
 --sounds
 local pe = Location:new(world, 1.2, 215.0, -22.0);
@@ -32,12 +32,10 @@ local p7 = Location:new(world, 1.3, 168.0, -267.0);
 local p8 = Location:new(world, 1.3, 168.0, -224.0);
 local p9 = Location:new(world, -0.7, 168.0, -223.0);
 local p9 = Location:new(world, -0.7, 168.0, -266.0);
-local p10 = Location:new(world, -0.7, 168.0, -310.0);
+local p10 = Location:new(world, -0.5, 168.0, -293.0);
 local p11 = Location:new(world, 3.6, 168.0, -234.0);
 local p12 = Location:new(world, 3.6, 168.0, -310.0);
 local p12 = Location:new(world, 3.6, 168.0, -367.0);
-
-
 
 --chests
 local endloot = Location:new(world2, 19536.0, 64.0, -20805.0);
@@ -45,10 +43,13 @@ local endloot = Location:new(world2, 19536.0, 64.0, -20805.0);
 --misc
 local Tunneltimer = false;
 
-----------------
---ai------------
-----------------
+--perms
+-- "runsafe.tunnel3.complete");
+-- "runsafe.tunnel3.loot");
 
+----------------
+----ai------------
+------------------
 local ai = '&f[&k?&r] &bDOG&f:'
 local Message = ''
 local ai = ''
@@ -65,25 +66,27 @@ function tunnel_whisper_good(npc, msg, player)
 	player:sendMessage('&f&c' .. npc .. '&f' .. msg);
 end
 
+function title(player, title, subtitle)
+         sendTitle(player.name, title, subtitle);
+end
+
 ------------------
---Respawning----------
-------------------
+----Respawning----------
+--------------------
 
 function respawn_tunnel(data)
              local player = Player:new(data.player);
              player:setHealth(20);
              player:teleport(tunnelrespawn);
-	     player:sendMessage("&7You feel a little more crazy...");
+             player:sendMessage("&cyou feel something strange...");
 
       end
-   end
 
-registerHook("PLAYER_DEATH", "respawn_tunnel", "tunnel2");
-
+--registerHook("PLAYER_DEATH", "respawn_tunnel", "tunnel2");
 
 ------------------
---Entrance----------
-------------------
+----Entrance----------
+--------------------
 
 function tunnel_start(data)
 	local p = Player:new(data["player"]);
@@ -93,7 +96,7 @@ end
 registerHook("REGION_ENTER", "tunnel_start", "tunnel2-tunnel3_main");
 
 function tunnel_tp_enter(data)
-	local p = Player:new(data["player"]);
+	local player = Player:new(data["player"]);
 	player:teleport(tunnelenter);
 
 end
@@ -103,77 +106,83 @@ registerHook("REGION_ENTER", "tunnel_tp_enter", "survival3-tunnel_e");
 function tunnel_e_message(data)
 	local p = Player:new(data["player"]);
 	p:sendMessage("&4&n Yo&ku&r&4&nr &kf&r&4&nate i&ks&r&4&n se&kal&r&4&ned, yo&ku&r&4&n can n&ke&r&4&nver &kes&r&4&ncape");
+        pe:playSound('ENTITY_ENDERMEN_AMBIENT', 5, 0.5);
         p:sendEvent("achievement.wrongofpassage");
-        pe:playSound('ENTITY_ENDERMEN_AMBIENT', 1, 0.5);
+        p:setMode("ADVENTURE");
 end
 
 registerHook("REGION_ENTER", "tunnel_e_message", "tunnel2-tunnel3_e_message");
 
-
-
-
 ------------------
---Part1----------
-------------------
+----Part1----------
+--------------------
 
 ------------------
---Part2----------
-------------------
+----Part2----------
+--------------------
+
+function hardcore_mode(data)
+        local p = Player:new(data["player"]);
+        p:sendMessage("&c Gamemode has been set to &4HARDCORE&c.");
+        p :setHealth(0.5);
+
+end
+
+registerHook("INTERACT", "hardcore_mode", 77, "tunnel2", 1.0, 170.0, -48.0);
 
 ------------------
---Part3----------
-------------------
+----Part3----------
+--------------------
 
 function plate_1(data)
-        local p = Player:new(data["player"]);
-        p :setHealth(0);
+	local p = Player:new(data["player"]);
+	p :setHealth(0);
 end
 
 function plate_2(data)
-        local player = Player:new(data["player"]);
-        EventEngine.player.addPotionEffect(player.name, 'BLINDNESS', 10, 5);
+	local player = Player:new(data["player"]);
+	EventEngine.player.addPotionEffect(player.name, 'BLINDNESS', 10, 5);
 end
 
 function plate_3(data)
-        local player = Player:new(data["player"]);
-        EventEngine.player.addPotionEffect(player.name, 'POISON', 10, 10);
+	local player = Player:new(data["player"]);
+	EventEngine.player.addPotionEffect(player.name, 'POISON', 10, 10);
 end
 
 function plate_4(data)
-        local player = Player:new(data["player"]);
-        EventEngine.player.addPotionEffect(player.name, 'WITHER', 10, 10);
+	local player = Player:new(data["player"]);
+	EventEngine.player.addPotionEffect(player.name, 'WITHER', 10, 10);
 end
 
 function plate_5(data)
-        local player = Player:new(data["player"]);
-        EventEngine.player.addPotionEffect(player.name, 'SLOW', 1, 10);
+	local player = Player:new(data["player"]);
+	EventEngine.player.addPotionEffect(player.name, 'SLOW', 1, 10);
 end
 
 function plate_6(data)
-        local player = Player:new(data["player"]);
-        EventEngine.player.addPotionEffect(player.name, 'CONFUSION', 100, 10);
+	local player = Player:new(data["player"]);
+	EventEngine.player.addPotionEffect(player.name, 'CONFUSION', 100, 10);
 end
 
 registerHook("INTERACT", "plate_1", 70, "tunnel2", -1.0, 168.0, -137.0);
-registerHook("INTERACT", "plate_2", 70, "tunnel2", 2.0, 168.0, -136.0);
-registerHook("INTERACT", "plate_3", 70, "tunnel2", 3.0, 168.0, -134.0);
+registerHook("INTERACT", "plate_3", 70, "tunnel2", 2.0, 168.0, -136.0);
+registerHook("INTERACT", "tunnel_tp_enter", 70, "tunnel2", 3.0, 168.0, -134.0);
 registerHook("INTERACT", "plate_4", 70, "tunnel2", -1.0, 168.0, -134.0);
-registerHook("INTERACT", "plate_6", 70, "tunnel2", 1.0, 168.0, -135.0);
-registerHook("INTERACT", "tunnel_tp_enter", 70, "tunnel2", 3.0, 168.0, -141.0);
-registerHook("INTERACT", "plate_3", 70, "tunnel2", 0.0, 168.0, -141.0);
+registerHook("INTERACT", "plate_2", 70, "tunnel2", 1.0, 168.0, -135.0);
+registerHook("INTERACT", "plate_6", 70, "tunnel2", 3.0, 168.0, -141.0);
+registerHook("INTERACT", "plate_2", 70, "tunnel2", 0.0, 168.0, -141.0);
 registerHook("INTERACT", "plate_5", 70, "tunnel2", 0.0, 168.0, -139.0);
-registerHook("INTERACT", "plate_4", 70, "tunnel2", 1.0, 168.0, -139.0);
-registerHook("INTERACT", "plate_6", 70, "tunnel2", 3.0, 168.0, -136.0);
-  
+registerHook("INTERACT", "plate_1", 70, "tunnel2", 1.0, 168.0, -139.0);
+registerHook("INTERACT", "plate_3", 70, "tunnel2", 3.0, 168.0, -136.0);
+
 ------------------
---Part4----------
-------------------
+----Part4----------
+--------------------
 
 function tunnel3_message2(data)
 	local p = Player:new(data["player"]);
 	p:sendMessage("&4(???)&fYou should not be here... you cannot be me...");
-        p4:playSound('ENTITY_SKELETON_HORSE_HURT', 1, 0.5);
-
+--        p4:playSound('ENTITY_SKELETON_HORSE_HIT', 1, 0.5);
 end
 
 registerHook("REGION_LEAVE", "tunnel3_message2", "tunnel2-t3_message2");
@@ -183,7 +192,7 @@ function tunnel_reset1(data)
 	player:teleport(tunnelreset1);
         EventEngine.player.addPotionEffect(player.name, 'BLINDNESS', 10, 2);
         EventEngine.player.addPotionEffect(player.name, 'SLOW', 1, 1);
-        p6:playSound('AMBIENCE_CAVE', 1, 0.5);
+--        p6:playSound('AMBIENCE_CAVE', 1, 0.5);
 
 end
 
@@ -191,7 +200,7 @@ function tunnel_reset2(data)
 	local player = Player:new(data["player"]);
         EventEngine.player.addPotionEffect(player.name, 'BLINDNESS', 10, 2);
         EventEngine.player.addPotionEffect(player.name, 'SLOW', 1, 1);
-        p7:playSound('AMBIENCE_CAVE', 1, 0.5);
+--        p7:playSound('AMBIENCE_CAVE', 1, 0.5);
 
 end
 
@@ -202,7 +211,7 @@ function tunnel_reset3(data)
 	player:teleport(tunnelreset2);
         EventEngine.player.addPotionEffect(player.name, 'BLINDNESS', 10, 2);
         EventEngine.player.addPotionEffect(player.name, 'SLOW', 1, 1);
-        p10:playSound('AMBIENCE_CAVE', 1, 0.5);
+--        p10:playSound('AMBIENCE_CAVE', 1, 0.5);
 
 end
 
@@ -214,8 +223,10 @@ registerHook("REGION_ENTER", "tunnel_reset2", "tunnel2-t3_reset5");
 registerHook("REGION_ENTER", "tunnel_reset2", "tunnel2-t3_reset8");
 registerHook("REGION_ENTER", "tunnel_reset2", "tunnel2-t3_reset9");
 registerHook("REGION_ENTER", "tunnel_reset3", "tunnel2-t3_reset6");
+
+
 ------------------
---Maze----------
+----Maze----------
 ------------------
 
 local maze1 = Location:new(world, -5.300, 168.0, -152.554);
@@ -223,8 +234,8 @@ maze1:setYaw(89.4);
 maze1:setPitch(6.2);
 
 local maze2 = Location:new(world, -7.592, 165.0, -152.355);
-maze2:setYaw(91.1);
-maze2:setPitch(9.5);
+maze2:setYaw(70.1);
+maze2:setPitch(6.5);
 
 local maze3 = Location:new(world, -7.592, 162.0, -152.355);
 maze3:setYaw(91.1);
@@ -233,7 +244,6 @@ maze3:setPitch(9.5);
 local maze4 = Location:new(world, -7.592, 159.0, -152.355);
 maze4:setYaw(91.1);
 maze4:setPitch(9.5);
-
 
 local maze5 = Location:new(world, -7.592, 154.0, -152.355);
 maze5:setYaw(91.1);
@@ -283,10 +293,10 @@ end
 function maze_cheeve(data)
 	local p = Player:new(data["player"]);
 	p:sendEvent("achievement.wrongturn");
-
+--        p:sendMessage("&7Wrong Turn Cheeve Granted - Debug");
 end
 
---Bad Warps
+--bad warps
 registerHook("REGION_ENTER", "maze_enter", "tunnel2-maze1");
 registerHook("REGION_ENTER", "maze_enter", "tunnel2-maze2");
 registerHook("REGION_ENTER", "maze_enter", "tunnel2-maze3");
@@ -303,7 +313,7 @@ registerHook("REGION_ENTER", "maze_5", "tunnel2-maze18");
 registerHook("REGION_ENTER", "maze_5", "tunnel2-maze19");
 registerHook("REGION_ENTER", "maze_5", "tunnel2-maze20");
 
---Good Warps
+--good warps
 registerHook("REGION_ENTER", "maze_2", "tunnel2-maze4");
 registerHook("REGION_ENTER", "maze_3", "tunnel2-maze8");
 registerHook("REGION_ENTER", "maze_4", "tunnel2-maze12");
@@ -313,81 +323,35 @@ registerHook("REGION_ENTER", "maze_6", "tunnel2-maze17");
 --Cheeve
 registerHook("REGION_ENTER", "maze_cheeve", "tunnel2-maze_cheeve");
 
-
 ------------------
---End----------
-------------------
+----End----------
+--------------------
 
 local sign = Location:new(world, -2.0, 170.0, -678.0);
-local book = Location:new(world, 3.0, 163.0, -681.0);
-
-local bookChestPlayers = {};
-local bookChestResetTimer = Timer:new("book_reset_chest", 200 * 600 * 50);
-local bookChestResetTimerRunning = false;
-local bookChestOpen = Location:new(world, 3.0, 163.0, -681.0);
-
-function book_reset_chest()
-	 bookChestPlayers = {};
-	 bookChestResetTimerRunning = false;
-end
 
 function tunnel3_message3(data)
-	local p = Player:new(data["player"]);
-	p:sendMessage("&4(???)&fStop! I will not let you go any further... you're hurting us both. GET OUT!!!");
-        p:sendEvent("achievement.legacyofpain");
-        p5:playSound('ENTITY_ZOMBIE_PIG_DEATH', 1, 0.5);
-        sign:setSign('Recent Survivor:', p.name, 'Time:', 'n/a');
+	local player = Player:new(data.player);
+	player:sendMessage("&4(???)&fStop! I will not let you go any further... you're hurting us both. GET OUT!!!");
+        player:sendEvent("achievement.legacyofpain");
+        sign:setSign('Recent Survivor:', player.name, 'Time:', '2h3?m??s');
+	player:addPermission("runsafe.tunnel3.complete");
+	player:addPermission("runsafe.tunnel3.loot");
+	player:sendMessage("&6You'll get your rewards for completing tunnel 3 once you return to spawn.");
 end
 
 registerHook("REGION_LEAVE", "tunnel3_message3", "tunnel2-t3_message3");
 
-function book_1(data)
-        local player = Player:new(data.player);
-        if not  bookChestPlayers[player.name] then
-                book:cloneChestToPlayer(player.name);
-                player:closeInventory();
-                bookChestPlayers[player.name] = true;
-		player:sendMessage("&f Sign the book and leave it in the hopper to get your name on the wall.");
-	        player:sendMessage("&f You'll get your rewards once you leave the tunnel.");
-
-                if not bookChestResetTimerRunning then
-                        bookChestResetTimerRunning = true;
-                        bookChestResetTimer:start();
-                end
-        end
-end
-
-registerHook("INTERACT", "book_1", 77, "tunnel2", -2.0, 169.0, -677.0);
-registerHook("INTERACT", "book_1", 77, "tunnel2", -2.0, 169.0, -679.0);
-
 function tunnel3_endloot(data)
         local player = Player:new(data.player);
+	  if player:hasPermission("runsafe.tunnel3.loot") then
               endloot:cloneChestToPlayer(player.name);
               player:closeInventory();
-  
-registerHook("REGION_LEAVE", "tunnel3_endloot", "tunnel-tunnel3_test");
-
-function cheeve1(data)
-	local p = Player:new(data["player"]);
-        p:sendEvent("achievement.wrongofpassage");
-	p:sendMessage("&7Wrong Of Passage Cheeve Granted - Debug");
+	      player:removePermission("runsafe.tunnel3.loot");
+	end
 end
+ 
+registerHook("REGION_ENTER", "tunnel3_endloot", "survival3-spawn");
 
-function cheeve2(data)
-	local p = Player:new(data["player"]);
-        p:sendEvent("achievement.wrongturn");
-	p:sendMessage("&7Wrong Turn Cheeve Granted - Debug");
-end
-
-function cheeve3(data)
-	local p = Player:new(data["player"]);
-         p:sendEvent("achievement.legacyofpain");
-         p:sendMessage("&7Legacy of Pain Cheeve Granted - Debug");
-end
-
-
-
-registerHook("INTERACT", "cheeve1", 143, "tunnel2", 4.0, 175.0, 2.0);
-registerHook("INTERACT", "cheeve2", 143, "tunnel2", 4.0, 175.0, 4.0);
-registerHook("INTERACT", "cheeve3", 143, "tunnel2", 4.0, 175.0, 6.0);
-  
+------------------
+---- Tunnel 4 ----
+------------------
