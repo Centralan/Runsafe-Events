470941894a0920cf650aa616bb9636f3b3ff8e29
Centralan <Jessegauthier070296@gmail.com>
2024-02-21 15:24:38 -0500

    Update temple.lua

--- temple.lua
+++ temple.lua
@@ -609,7 +609,7 @@ function temple_emerarld_check2(data)
           if not player:hasPermission("runsafe.temple.emerald") then
 	         player:setHealth(0);
 		 tfentersign:setSign('Latest', 'Explorer:', '', player.name);
-                 player:sendMessage("&4[D] &bTemple Sentinel&f: You have not yet proven your worth..");
+--                 player:sendMessage("&4[D] &bTemple Sentinel&f: You have not yet proven your worth..");
 		 templelightningE:lightningStrike();
 	         worldsound:playSound('ENTITY_LIGHTNING_THUNDER', 10000, 1);
                  templespawnsound:playSound('ENTITY_LIGHTNING_THUNDER', 10000, 1);
